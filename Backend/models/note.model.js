4a636b59ddfd5a8ad0863475df8c26888d7b6848
Zied Hedhiri <zied.hedhiri@esprit.tn>
2022-07-28 16:39:20 +0100

    html finetuning

--- Backend/models/note.model.js
+++ Backend/models/note.model.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
 var noteSchema = new mongoose.Schema({
-    Designation: {type:String, minLength:5 , maxLength:20, required: true },
-    nomEnseignant: {type:String, minLength:5 , maxLength:20, required: true }, 
+    Designation: {type:String, minLength:2 , maxLength:20, required: true },
+    nomEnseignant: {type:String, minLength:3 , maxLength:20, required: true }, 
     noteCc:{type: Number , min:0,max: 20 , required: false },
     noteExam:{type: Number,min:0,max: 20 , required: true},
+    EmailE:String,
+    EmailP:String,
     
     imageUrl:String
 });
